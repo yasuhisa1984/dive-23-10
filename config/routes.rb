9d0eb71101d81bf507eef122c6ffa3d46bfb7bca
yuto <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-05-22 16:30:04 +0000

    generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources 'users', only: [:index, :show]
+  resources 'answers'
+  resources 'questions'
+
+  get 'favorites/index'
+  get 'ranking/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
