951a04987ea39cb7b159a456f63a1c5f07a113fd
yuto <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-05-24 12:53:02 +0000

    create vote table

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
+  has_many :votes, dependent: :destroy
 end
---
+++ spec/factories/votes.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :vote do
+    user nil
+    question nil
+    answer nil
+  end
+end
