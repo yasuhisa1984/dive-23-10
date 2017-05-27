5e7b097a346d18b755becadfece7234000aee791
yuto <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-05-27 07:52:38 +0000

    create tagging model, tag form and tags index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
 
   get 'favorites/index'
   get 'ranking/index'
+  get 'tags/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
