3a609d8055c6638a96bd976a8701f08dc17c7695
小鍛冶泰久 <penmouse275@nifty.com>
2017-05-27 15:17:14 +0900

    fix refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def profile_img(user)
     if user.avatar?
-      return image_tag(user.avatar, alt: user.name, :size => "164x164")
+      image_tag(user.avatar, alt: user.name, :size => "164x164")
     else
       img_url = 'no_image.png'
       image_tag(img_url, alt: user.name, :size => "164x164")
