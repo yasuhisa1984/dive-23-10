b8da8360a8a7a3c73c6f84a1bf261e9eeb2f751c
小鍛冶泰久 <penmouse275@nifty.com>
2017-05-28 14:30:25 +0900

    fix vote migrate

--- db/migrate/20170523121833_create_votes.rb
+++ db/migrate/20170523121833_create_votes.rb
@@ -1,10 +1,11 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.references :user, index: true, foreign_key: true, null: false, unique: true
-      t.references :question, index: true, foreign_key: true, null: false, unique: true
-      t.references :answer, index: true, foreign_key: true, null: false, unique: true
+      t.references :user, index: true, foreign_key: true, null: false
+      t.references :question, index: true, foreign_key: true, null: false
+      t.references :answer, index: true, foreign_key: true, null: false
       t.timestamps null: false
     end
+     add_index :votes, [:user_id, :question_id, :answer_id], :unique => true
   end
 end
