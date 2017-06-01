985dfbbfe9510a9439c5f43db534fc00fff8ff99
Atsushi Nakagawa <Atsushi@Atsushi-no-MacBook-Pro.local>
2017-06-01 21:17:39 +0900

    vote to answer function

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,33 +1,68 @@
 class VotesController < ApplicationController
+#長くなりすぎなのでリファクタリングしたい
   def create
-    @question = params[:question_id]
-    if current_user.question_voting_plus_or_minus_or_nil(@question)
-      @vote = Vote.find(params[:id])
-      @vote.destroy
-      redirect_to question_path(@question)
-    else
-      @question = Question.find(params[:question_id])
-      if params[:is_up] == "true"
-        plus_or_minus = 1
+    if params[:question_or_answer] == "question"
+      @question = params[:question_id]
+      if current_user.question_voting_plus_or_minus_or_nil(@question)
+        @vote = Vote.find(params[:id])
+        @vote.destroy
+        redirect_to question_path(@question)
       else
-        plus_or_minus = -1
+        @question = Question.find(params[:question_id])
+        if params[:is_up] == "true"
+          plus_or_minus = 1
+        else
+          plus_or_minus = -1
+        end
+        Vote.create(user_id: current_user.id, question_id: @question.id, plus_or_minus: plus_or_minus)
+        redirect_to question_path(@question)
       end
-      Vote.create(user_id: current_user.id, question_id: @question.id, plus_or_minus: plus_or_minus)
-      redirect_to question_path(@question)
+    else
+      @answer = Answer.find(params[:answer_id])
+      @question = @answer.question
+      if current_user.answer_voting_plus_or_minus_or_nil(@answer)
+        @vote = Vote.find(params[:id])
+        @vote.destroy
+        redirect_to question_path(@question)
+      else
+        @answer = Answer.find(params[:answer_id])
+        if params[:is_up] == "true"
+          plus_or_minus = 1
+        else
+          plus_or_minus = -1
+        end
+        Vote.create(user_id: current_user.id, answer_id: @answer.id, plus_or_minus: plus_or_minus)
+        redirect_to question_path(@question)
+      end
     end
   end
 
   def destroy
-    @question = params[:question_id]
-    if current_user.question_voting_plus_or_minus_or_nil(@question)
-      @vote = Vote.find(params[:id])
-      @vote.destroy
-      redirect_to question_path(@question)
+    if params[:question_or_answer] == "question"
+      @question = params[:question_id]
+      if current_user.question_voting_plus_or_minus_or_nil(@question)
+        @vote = Vote.find(params[:id])
+        @vote.destroy
+        redirect_to question_path(@question)
+      else
+        @question = Question.find(params[:question_id])
+        plus_or_minus = -1
+        Vote.create(user_id: current_user.id, question_id: @question.id, plus_or_minus: plus_or_minus)
+        redirect_to question_path(@question)
+      end
     else
-      @question = Question.find(params[:question_id])
-      plus_or_minus = -1
-      Vote.create(user_id: current_user.id, question_id: @question.id, plus_or_minus: plus_or_minus)
-      redirect_to question_path(@question)
+      @answer = Answer.find(params[:answer_id])
+      @question = @answer.question
+      if current_user.answer_voting_plus_or_minus_or_nil(@answer)
+        @vote = Vote.find(params[:id])
+        @vote.destroy
+        redirect_to question_path(@question)
+      else
+        @answer = Answer.find(params[:answer_id])
+        plus_or_minus = -1
+        Vote.create(user_id: current_user.id, answer_id: @answer.id, plus_or_minus: plus_or_minus)
+        redirect_to question_path(@question)
+      end
     end
   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,8 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   has_many :votes, dependent: :destroy
+
+  def voted_point
+    self.votes.sum(:plus_or_minus)
+  end
 end
