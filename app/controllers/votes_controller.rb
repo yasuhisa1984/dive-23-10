8084d8822082a1b55796c9018d1fae345a4198d2
Atsushi Nakagawa <Atsushi@Atsushi-no-MacBook-Pro.local>
2017-06-02 22:21:28 +0900

    change vote-button to Ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,13 @@
 class VotesController < ApplicationController
 #長くなりすぎなのでリファクタリングしたい
+  respond_to :js
   def create
     if params[:question_or_answer] == "question"
       @question = Question.find(params[:question_id])
       if current_user.question_voting_plus_or_minus_or_nil(@question)
         @vote = Vote.find(params[:id])
         @vote.destroy
-        redirect_to question_path(@question)
+        respond_with @question, template: 'votes/vote_question'
       else
         @question = Question.find(params[:question_id])
         if params[:is_up] == "true"
@@ -15,15 +16,15 @@ class VotesController < ApplicationController
           plus_or_minus = -1
         end
         Vote.create(user_id: current_user.id, question_id: @question.id, plus_or_minus: plus_or_minus)
-        redirect_to question_path(@question)
+        respond_with @question, template: 'votes/vote_question'
       end
-    else
+    else #params[:question_or_answer] !== "question"
       @answer = Answer.find(params[:answer_id])
       @question = @answer.question
       if current_user.answer_voting_plus_or_minus_or_nil(@answer)
         @vote = Vote.find(params[:id])
         @vote.destroy
-        redirect_to question_path(@question)
+        respond_with @answer, template: 'votes/vote_answer'
       else
         @answer = Answer.find(params[:answer_id])
         if params[:is_up] == "true"
@@ -32,7 +33,7 @@ class VotesController < ApplicationController
           plus_or_minus = -1
         end
         Vote.create(user_id: current_user.id, answer_id: @answer.id, plus_or_minus: plus_or_minus)
-        redirect_to question_path(@question)
+        respond_with @answer, template: 'votes/vote_answer'
       end
     end
   end
@@ -43,25 +44,25 @@ class VotesController < ApplicationController
       if current_user.question_voting_plus_or_minus_or_nil(@question)
         @vote = Vote.find(params[:id])
         @vote.destroy
-        redirect_to question_path(@question)
+        respond_with @question, template: 'votes/vote_question'
       else
         @question = Question.find(params[:question_id])
         plus_or_minus = -1
         Vote.create(user_id: current_user.id, question_id: @question.id, plus_or_minus: plus_or_minus)
-        redirect_to question_path(@question)
+        respond_with @question, template: 'votes/vote_question'
       end
-    else
+    else #params[:question_or_answer] !== "question"
       @answer = Answer.find(params[:answer_id])
       @question = @answer.question
       if current_user.answer_voting_plus_or_minus_or_nil(@answer)
         @vote = Vote.find(params[:id])
         @vote.destroy
-        redirect_to question_path(@question)
+        respond_with @answer, template: 'votes/vote_answer'
       else
         @answer = Answer.find(params[:answer_id])
         plus_or_minus = -1
         Vote.create(user_id: current_user.id, answer_id: @answer.id, plus_or_minus: plus_or_minus)
-        redirect_to question_path(@question)
+        respond_with @answer, template: 'votes/vote_answer'
       end
     end
   end
