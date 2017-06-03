8ac9e4130a57495c7e52983957a3f8acbf253a7f
sa <pps6777@gmail.com>
2017-06-03 14:54:22 +0900

    Delete size of profile image (for CSS)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
 
   def profile_img(user)
     if user.avatar?
-      image_tag(user.avatar, alt: user.name, :size => "164x164")
+      image_tag(user.avatar, alt: user.name)
     else
       img_url = 'no_image.png'
-      image_tag(img_url, alt: user.name, :size => "164x164")
+      image_tag(img_url, alt: user.name)
     end
   end
 
