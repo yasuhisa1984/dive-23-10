bf5e636916f490c30acf1d1b47ad6bcdcc72fd13
小鍛冶泰久 <penmouse275@nifty.com>
2017-06-04 15:04:54 +0900

    fix N + 1 question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
+
   before_action :set_question, only: [:edit, :update, :show, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  
   def index
     @questions = Question.all.order(created_at: :desc).includes(:user, :tags)
   end
@@ -17,15 +17,23 @@ class QuestionsController < ApplicationController
       tag = Tag.register!(name)
       @question.taggings.build(tag_id: tag.id)
     end
-    @question.save
-    redirect_to questions_path
-    NoticeMailer.sendmail_question(@question).deliver
+    if @question.save
+      redirect_to questions_path, notice:"質問を投稿しました"
+      NoticeMailer.sendmail_question(@question).deliver
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
   def update
+    @question.tags.destroy_all
+    input_tags_names.each do |name|
+      tag = Tag.register!(name)
+      @question.taggings.build(tag_id: tag.id)
+    end
     if @question.update(question_params)
       redirect_to questions_path, notice: '質問を更新しました'
     else
